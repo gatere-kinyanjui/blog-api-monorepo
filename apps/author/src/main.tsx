a40e9863cba2083dd8f2237a219ebc7fe5d844fc
Gatere Kinyanjui <gaterekinyanjui@gmail.com>
2025-05-27 18:13:11 +0300

    feat: add rich text editor

--- apps/author/src/main.tsx
+++ apps/author/src/main.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import CreatePost from "./pages/create-post/createPost.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <CreatePost />
   </StrictMode>
 );
--- apps/author/src/pages/create-post/createPost.tsx
+++ apps/author/src/pages/create-post/createPost.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import EditorDemo from "@/components/editor";
+import { RichTextEditorDemo } from "@/components/rich-editor";
 
 export default function CreatePost() {
-  return <EditorDemo />;
+  return <RichTextEditorDemo />;
 }
