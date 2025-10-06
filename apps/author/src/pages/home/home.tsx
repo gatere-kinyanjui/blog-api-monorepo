f6d878b26fa3a2c18d26398f5087369ce4337574
gatere-kinyanjui <gaterekinyanjui@gmail.com>
2025-10-06 21:39:24 +0300

    rename author to frontend-author

--- apps/author/src/pages/home/home.tsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { TypographyDemo } from "@/components/typography-demo";
-import { ViewPost } from "@/components/view-post";
-
-export default function Home() {
-  return <ViewPost />;
-}
